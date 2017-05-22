4815e6a38ebd63640f91c342ab381195c52ee228
ezsilmar <zhirov@skbkontur.ru>
2017-05-22 11:24:01 +0500

    Checked build, fixed indentation

--- sorter/sorter.ino
+++ sorter/sorter.ino
@@ -31,92 +31,92 @@ void setup()
     pinMode(SERVO_MM_PUSH_OUT, OUTPUT);
     ServoMMPush.attach(SERVO_MM_PUSH_OUT);
     ServoMMPush.write(SERVO_MM_PUSH_STATE_WAITING);
-	
-	pinMode(SERVO_GUTTER_OUT, OUTPUT);
+    
+    pinMode(SERVO_GUTTER_OUT, OUTPUT);
     ServoGutter.attach(SERVO_GUTTER_OUT);
 }
 
 void loop() 
 {
-	// Initialize color sensor
-	ColorSensor = new MD_TCS230(S2_OUT, S3_OUT, S0_OUT, S1_OUT);
+    // Initialize color sensor
+    ColorSensor = new MD_TCS230(S2_OUT, S3_OUT, S0_OUT, S1_OUT);
     ColorSensor->begin();
-	
-	// Reading data from color sensor
+    
+    // Reading data from color sensor
     sensorData sd;
     ColorSensor->read();
     while (!ColorSensor->available())
         ;
     ColorSensor->getRaw(&sd);
-	uint32_t clean = read_clean();
-	
-	// Call generated classifier to detect m&m color
-	int color = make_a_decision((float) sd.value[TCS230_RGB_R], (float) sd.value[TCS230_RGB_G], (float) sd.value[TCS230_RGB_B], (float) clean);
+    uint32_t clean = read_clean();
+    
+    // Call generated classifier to detect m&m color
+    int color = make_a_decision((float) sd.value[TCS230_RGB_R], (float) sd.value[TCS230_RGB_G], (float) sd.value[TCS230_RGB_B], (float) clean);
     print_debug_output(&sd, clean, color);
-	
-	// Delete color sensor. It somehow conflicts with servos and disposing helps
+    
+    // Delete color sensor. It somehow conflicts with servos and disposing helps
     delete ColorSensor;
-	
-	// Initialize servos
+    
+    // Initialize servos
     ServoMMPush.attach(SERVO_MM_PUSH_OUT);
     ServoGutter.attach(SERVO_GUTTER_OUT);
-	
-	// White means there is no m&m on sensor
-	// Used a color name to simplify python code generator
+    
+    // White means there is no m&m on sensor
+    // Used a color name to simplify python code generator
     if (color != white)
     {
       move_gutter(color);
-	  delay(100);
+      delay(100);
       push_mm();
     }
-	delay(MM_ITERATION_DELAY);
+    delay(MM_ITERATION_DELAY);
     
-	// Dispose servos
-	ServoMMPush.detach();
+    // Dispose servos
+    ServoMMPush.detach();
     ServoGutter.detach();
 }
 
 uint32_t read_clean()
 {
-	ColorSensor->setEnable(true);
+    ColorSensor->setEnable(true);
     ColorSensor->setFilter(TCS230_RGB_X);
-	uint32_t clean = ColorSensor->readSingle();
+    uint32_t clean = ColorSensor->readSingle();
     ColorSensor->setEnable(false);
-	return clean;
+    return clean;
 }
 
 void push_mm()
 {
-  Serial.println("Pushing");
-  for (int i = SERVO_MM_PUSH_STATE_WAITING; i < SERVO_MM_PUSH_STATE_ACTING; i += 50)
-  {
-    ServoMMPush.write(i);
+    Serial.println("Pushing");
+    for (int i = SERVO_MM_PUSH_STATE_WAITING; i < SERVO_MM_PUSH_STATE_ACTING; i += 50)
+    {
+      ServoMMPush.write(i);
+      delay(25);
+    }
+    ServoMMPush.write(SERVO_MM_PUSH_STATE_ACTING);
     delay(25);
-  }
-  ServoMMPush.write(SERVO_MM_PUSH_STATE_ACTING);
-  delay(25);
-  ServoMMPush.write(SERVO_MM_PUSH_STATE_WAITING);
+    ServoMMPush.write(SERVO_MM_PUSH_STATE_WAITING);
 }
 
 void move_gutter(int sector)
 {
-	int oneSectorLen = (SERVO_GUTTER_STATE_MAX - SERVO_GUTTER_STATE_MIN)/SERVO_GUTTER_SECTORS_COUNT
-	int value = SERVO_GUTTER_STATE_MIN + sector*oneSectorLen;
-	ServoGutter.write(value);
-	Serial.print("Gutter moves to ");
-	Serial.println(value);
+    int oneSectorLen = (SERVO_GUTTER_STATE_MAX - SERVO_GUTTER_STATE_MIN)/SERVO_GUTTER_SECTORS_COUNT;
+    int value = SERVO_GUTTER_STATE_MIN + sector*oneSectorLen;
+    ServoGutter.write(value);
+    Serial.print("Gutter moves to ");
+    Serial.println(value);
 }
 
 void print_debug_output(sensorData* sd, uint32_t clean, int color)
 {
-	Serial.print(sd->value[TCS230_RGB_R]);
-	Serial.print(" ");
-	Serial.print(sd->value[TCS230_RGB_G]);
-	Serial.print(" ");
-	Serial.print(sd->value[TCS230_RGB_B]);
-	Serial.print(" ");
-	Serial.print(clean);
-	Serial.print(" ");
-	Serial.print(color);
-	Serial.println();
+    Serial.print(sd->value[TCS230_RGB_R]);
+    Serial.print(" ");
+    Serial.print(sd->value[TCS230_RGB_G]);
+    Serial.print(" ");
+    Serial.print(sd->value[TCS230_RGB_B]);
+    Serial.print(" ");
+    Serial.print(clean);
+    Serial.print(" ");
+    Serial.print(color);
+    Serial.println();
 }
